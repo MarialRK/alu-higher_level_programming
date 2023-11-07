55cb8733d7ae345b63fc56ab9ff4c9bdf964ee91
danielmarial <d.kudum@alustudent.com>
2023-11-07 05:36:05 -0800

    I am a programmer at ALU

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -5,7 +5,6 @@ def no_c(my_string):
         if char != 'c' and char != 'C':
             result += char
     return result
-
 if _name_ == "_main_":
     print(no_c("Best School"))
     print(no_c("Chicago"))
