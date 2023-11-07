03f19128ed55181b94758fd48c9c9553759d0063
danielmarial <d.kudum@alustudent.com>
2023-11-07 13:20:12 -0800

    amen

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return list(map(lambda x: list(map(lambda i: i ** 2, x)), matrix))
-15)
-#!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     for key in list(a_dictionary):
         if a_dictionary[key] == value:
