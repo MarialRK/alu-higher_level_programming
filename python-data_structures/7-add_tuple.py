40a50e6219b2e814f8463230a72ed202f4611872
danielmarial <d.kudum@alustudent.com>
2023-11-09 10:56:39 -0800

    i'm yours lord

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # If a tuple is smaller than 2, use 0 for each missing integer
-    if len(tuple_a) < 2:
-        tuple_a += (0, 0)
-    if len(tuple_b) < 2:
-        tuple_b += (0, 0)
-
-    # Add the first elements and the second elements of the two tuples
-    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-
-    return result
-
-if _name_ == "_main_":
-    tuple_a = (1, 89)
-    tuple_b = (88, 11)
-    new_tuple = add_tuple(tuple_a, tuple_b)
-    print(new_tuple)
-
-    print(add_tuple(tuple_a, (1,)))
-    print(add_tuple(tuple_a, ()))
+    aList = list(tuple_a)
+    bList = list(tuple_b)
+    for i in range(2):
+        aList.append(0)
+        bList.append(0)
+    sum_0 = aList[0] + bList[0]
+    sum_1 = aList[1] + bList[1]
+    tuple_c = (sum_0, sum_1)
+    return tuple_c7.
