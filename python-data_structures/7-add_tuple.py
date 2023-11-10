a2c3cc059a6dbc69520867d038720b6446725a86
danielmarial <d.kudum@alustudent.com>
2023-11-10 02:43:15 -0800

    amen

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+# 7-add_tuple.py
+
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    aList = list(tuple_a)
-    bList = list(tuple_b)
-    for i in range(2):
-        aList.append(0)
-        bList.append(0)
-    sum_0 = aList[0] + bList[0]
-    sum_1 = aList[1] + bList[1]
-    tuple_c = (sum_0, sum_1)
-    return tuple_c7.
+    """Add two tuples."""
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
