1f7f31587a1533cad592a4145c49ec87c51c35ba
damielmarial <d.kudum@alustudent.com>
2023-11-21 05:14:48 -0800

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-Square = __import__('0-square').Square
 
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+
+"""Example Google style docstrings."""
+
+
+class Square:
+    """The summary line for a class docstring should fit on one line."""
+    pass
