5b412fc3a1b755ac5d44ed1d5a72ae5323649907
damielmarial <d.kudum@alustudent.com>
2023-11-24 08:33:11 -0800

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-# script that adds all arguments to a Python list,
-# and then save them to the file//
-"""
-    script that adds all arguments to a Python list,
-    and save them to a file
-"""
-
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 if _name_ == "_main_":
