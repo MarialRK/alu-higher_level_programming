0df208781f075555150da7ea548331c262b4eb9c
damielmarial <d.kudum@alustudent.com>
2023-11-24 10:13:50 -0800

    10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-'''a class Student that defines a student'''
+"""Defines a class Student."""
 
 
 class Student:
-    '''student class'''
-    first_name = None
-    last_name = None
-    age = None
+    """Represent a student."""
 
-    def _init_(self, first_name, last_name, age):
-        '''inits the data needed'''
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        '''returns a dict rep of the data'''
-        context = {
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'age': self.age
-        }
-        if attrs is None or type(attrs) != list:
-            return context
-        else:
-            cont = {}
-            for item in attrs:
-                if type(item) != str:
-                    return context
-                if item in context.keys():
-                    cont[item] = context[item]
-            return cont
+        """Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
