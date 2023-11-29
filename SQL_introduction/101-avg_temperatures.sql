ae48330252433de0f45e59e6273603c7958254c8
damielmarial <d.kudum@alustudent.com>
2023-11-29 00:38:03 -0800

    101-avg_temperatures.sql

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,8 +1,6 @@
--- Displays the 3 cities with the highest average
--- temperatures between July and August.
+-- Displays the average temperature (in Fahrenheit)
+-- by city ordered by descending temperature.
 SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
-WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
-ORDER BY `avg_temp` DESC
-LIMIT 3;
+ORDER BY `avg_temp` DESC;
